a9542e29ad42429e1ee9ba05a74c134bb8906b57
okometsbu-beep <okometsbu@gmail.com>
2026-08-09 17:04:01 +0900

    Add TOPIK QUEST hot patch layer

---
+++ site-patch.js
@@ -0,0 +1,2 @@
+// TOPIK QUEST hot-patch layer.
+// The main index loads this file with a cache-busting query so future UI/logic updates can be shipped without replacing the large embedded content bundle.
