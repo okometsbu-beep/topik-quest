ccfad6562ea88b99c5598d2a5c3aedc0c7dda3eb
okometsbu-beep <okometsbu@gmail.com>
2026-08-29 18:06:43 +0900

    feat: make Travel exploration map-first

--- site-patch.js
+++ site-patch.js
@@ -1,8 +1,8 @@
-// MALBIT bootstrap v70
+// MALBIT bootstrap v71
 // Load the shared core, reviewed data, TOPIK I engine, then learning interactions.
 (function(){
   'use strict';
-  const VERSION='70';
+  const VERSION='71';
   const RUNTIME_FILES=Object.freeze([
     'site-patch-core.js',
     'storage-guard.js',
