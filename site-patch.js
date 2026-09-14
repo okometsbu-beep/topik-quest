9645ddea464f8d4b3921b0d94eb85685a0284b71
okometsbu-beep <okometsbu@gmail.com>
2026-09-14 18:09:03 +0900

    feat: add reviewed completion grammar shorts

--- site-patch.js
+++ site-patch.js
@@ -1,8 +1,8 @@
-// MALBIT bootstrap v113
+// MALBIT bootstrap v114
 // Load the shared core, reviewed data, TOPIK I engine, then learning interactions.
 (function(){
   'use strict';
-  const VERSION='113';
+  const VERSION='114';
   const RUNTIME_FILES=Object.freeze([
     'writing-answers.js',
     'site-patch-core.js',
