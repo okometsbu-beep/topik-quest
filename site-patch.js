cf5ec39bd0da6336aaf1a2ada2d89040e1e7db5e
okometsbu-beep <okometsbu@gmail.com>
2026-09-27 00:07:16 +0900

    chore: update site-patch.js for v140 draft preservation

--- site-patch.js
+++ site-patch.js
@@ -1,8 +1,8 @@
-// MALBIT bootstrap v139
+// MALBIT bootstrap v140
 // Load the shared core, reviewed data, TOPIK I engine, then learning interactions.
 (function(){
   'use strict';
-  const VERSION='139';
+  const VERSION='140';
   const RUNTIME_FILES=Object.freeze([
     'writing-answers.js',
     'site-patch-core.js',
